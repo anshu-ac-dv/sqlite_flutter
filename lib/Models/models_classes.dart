2084332c7c66007b18bd4a3fe2111ecc80dc15ae
anshu <anshu.ac.dv@gmail.com>
2026-02-10 17:32:31 +0530

    Initial Commit

--- lib/Models/models_classes.dart
+++ lib/Models/models_classes.dart
@@ -1,9 +1,9 @@
 class ModelsClasses {
-  final int id;
+  final int? id;
   final String name;
   final int age;
 
-  ModelsClasses({required this.name, required this.age, required this.id});
+  ModelsClasses({required this.name, required this.age, this.id});
 
   factory ModelsClasses.fromMap(Map<String, dynamic> map) {
     return ModelsClasses(id: map['id'], name: map['name'], age: map['age']);
--- lib/SQLite CURD/dynamic_data.dart
+++ lib/SQLite CURD/dynamic_data.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:sqlite_flutter/DB%20Handler/db_handler.dart';
+import 'package:path/path.dart';
+import 'package:sqlite_flutter/Models/models_classes.dart';
 
 class DynamicData extends StatelessWidget {
-  const DynamicData({super.key});
+  DynamicData({super.key});
+
+  final nameController = TextEditingController();
+  final ageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +17,61 @@ class DynamicData extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                hintText: "Enter Name",
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: ageController,
+              decoration: InputDecoration(
+                hintText: "Enter Age",
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                await DbHandler().insertData(
+                  ModelsClasses(
+                    name: nameController.text,
+                    age: int.parse(ageController.text),// '20' => 20
+                  ),
+                );
+                print("Data Inserted");
+              },
+              child: Text("Insert"),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                final data = await DbHandler().readData();
+                print(data[15]);
+              },
+              child: Text("Read"),
+            ),
+          ),
+        ],
       ),
     );
   }
